058f05bf60004127f1cceae61d1a5b9ec78c63e0
Miki Pregaard <48087675+Miki0110@users.noreply.github.com>
2022-04-14 21:47:44 +0200

    The detector can now detect trained data to be a specific type

--- Workshop/instrument_data.py
+++ Workshop/instrument_data.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import cv2 as cv
 import numpy as np
 import json
+from scipy.io import savemat
 
 # Simple function for diplaying image
 def resize_image(image, image_name, procent):
@@ -12,13 +13,11 @@ def resize_image(image, image_name, procent):
     cv.imshow(image_name, image)
     return
 
-
+# Simple functions for morphology
 def open_image(input_image, e_kernel, d_kernel):
     e_out = cv.erode(input_image, e_kernel)
     d_out = cv.dilate(e_out, d_kernel)
     return d_out
-
-
 def close_image(input_image, e_kernel, d_kernel):
     d_out = cv.dilate(input_image, d_kernel)
     e_out = cv.erode(d_out, e_kernel)
@@ -26,26 +25,39 @@ def close_image(input_image, e_kernel, d_kernel):
 
 
 class Features:
-    def __init__(self, contour, img):
+    # startup for initialising the features
+    def __init__(self, instrument_type):
+        self.type = instrument_type
+        self.circularity = []
+        self.compactness = []
+        self.elongation = []
+        self.thiness = []
+        self.intensity = []
+
+    # function for saving new contours into the training data
+    def save_cnt(self, contour, img):
 
         # Calculate feature properties
         area = cv.contourArea(contour)
         perimeter = cv.arcLength(contour, True)
 
         (min_x, min_y), (min_width, min_height), min_angle = cv.minAreaRect(contour)
-        #(ellipse_x, ellipse_y), (ellipse_MA, ellipse_ma), ellipse_angle = cv.fitEllipse(contour)
         (circle_x, circle_y), circle_radius = cv.minEnclosingCircle(contour)
 
-        self.circularity = 4 * np.pi * area / (perimeter**2)
-        self.compactness = area / (min_width * min_height)
-        self.elongation = min(min_width, min_height) / max(min_width, min_height)
-        #self.fatness = ellipse_ma / ellipse_MA
-        self.thiness = (4 * np.pi * area) / (perimeter**2)
+        self.circularity.append(4 * np.pi * area / (perimeter**2))
+        self.compactness.append(area / (min_width * min_height))
+        self.elongation.append(min(min_width, min_height) / max(min_width, min_height))
+        self.thiness.append((4 * np.pi * area) / (perimeter**2))
 
         # Intensity found in the image
         mask = np.zeros(img.shape, np.uint8)
         cv.drawContours(mask, [cnt], 0, 255, -1)
-        self.intensity = cv.mean(img, mask=mask)[0]
+        self.intensity.append((cv.mean(img, mask=mask)[0])/255)     # Divide by 255 to normalise
+
+    # used for analysing the data extracted from training data
+    def export_matlab(self, file_name):
+        savemat(file_name, {f'circularity': self.circularity, f'compactness': self.compactness,
+                            f'elongation': self.elongation, f'thiness': self.thiness, 'intensity': self.intensity})
 
 
 # importing the pictures
@@ -53,22 +65,28 @@ instruments = ['guitar', 'bass', 'trumpet']
 n_pics = 8
 
 for instrument in instruments:
+    # Initiate json files and Features class
     outfile = open(f'data_{instrument}.json', 'w')
+    f = Features(instrument)
     for i in range(n_pics):
         picture = cv.imread(f'{Path.cwd().as_posix()}/materialer/{instrument}/{instrument}{i+1}.jpg')
         grey = cv.imread(f'{Path.cwd().as_posix()}/materialer/{instrument}/{instrument}{i+1}.jpg', cv.IMREAD_GRAYSCALE)
 
+        # Image processing
         blur = cv.medianBlur(grey, 21)
         ret, threshold = cv.threshold(blur, 210, 255, cv.THRESH_BINARY_INV)
-        closed = close_image(threshold, cv.getStructuringElement(cv.MORPH_ELLIPSE, (15, 15)),cv.getStructuringElement(cv.MORPH_ELLIPSE, (35, 35)))
+        closed = close_image(threshold, cv.getStructuringElement(cv.MORPH_ELLIPSE, (15, 15)),
+                             cv.getStructuringElement(cv.MORPH_ELLIPSE, (35, 35)))
+
+        # Extract contours
         contours, hierarchy = cv.findContours(image=closed, mode=cv.RETR_EXTERNAL, method=cv.CHAIN_APPROX_NONE)
 
-        # image_copy = picture.copy()
-        # cv.drawContours(image_copy, contours, -1, (0, 255, 0), 3)
+        # Save the contours into the Features class
+        for cnt in contours:
+            f.save_cnt(cnt, grey)
 
-        for n in range(len(contours)):
-            cnt = contours[n]
-            f = Features(cnt, grey)
-            jsonStr = json.dumps(f.__dict__, indent=4)
-        outfile.write(jsonStr)
+    # write all the data into the .json and .mat files
+    jsonStr = json.dumps(f.__dict__, indent=4)
+    outfile.write(jsonStr)
+    f.export_matlab(f'data_{instrument}.mat')
 
--- Workshop/instrument_detector.py
+++ Workshop/instrument_detector.py
@@ -1,19 +1,40 @@
 from pathlib import Path
 import cv2 as cv
 import numpy as np
-from collections import deque
-from dataclasses import dataclass
+import json
 
 
 # Feature space
-@dataclass
 class Features:
-    contourIndex = int
-    area = int
-    perimeter = int
-    circularity = float
-    hasHoles = bool
-    elongation = float
+    # function for saving new contours into the training data
+    def __init__(self, contour, img):
+
+        # Calculate feature properties
+        area = cv.contourArea(contour)
+        perimeter = cv.arcLength(contour, True)
+
+        (min_x, min_y), (min_width, min_height), min_angle = cv.minAreaRect(contour)
+        (circle_x, circle_y), circle_radius = cv.minEnclosingCircle(contour)
+
+        self.circularity = (4 * np.pi * area / (perimeter**2))
+        self.compactness = (area / (min_width * min_height))
+        self.elongation = (min(min_width, min_height) / max(min_width, min_height))
+        self.thiness = ((4 * np.pi * area) / (perimeter**2))
+
+        # Intensity found in the image
+        mask = np.zeros(img.shape, np.uint8)
+        cv.drawContours(mask, [cnt], 0, 255, -1)
+        self.intensity = ((cv.mean(img, mask=mask)[0])/255)     # Divide by 255 to normalise
+
+    def return_vector(self):
+        feature_vector = []
+        feature_vector.append(self.circularity)
+        feature_vector.append(self.compactness)
+        feature_vector.append(self.elongation)
+        feature_vector.append(self.thiness)
+        feature_vector.append(self.intensity)
+        return feature_vector
+
 
 # A function that allows me to display an image an resize it
 def resize_image(image, image_name, procent):
@@ -24,32 +45,70 @@ def resize_image(image, image_name, procent):
     cv.imshow(image_name, image)
 
 
+# Simple functions for morphology
+def open_image(input_image, e_kernel, d_kernel):
+    e_out = cv.erode(input_image, e_kernel)
+    d_out = cv.dilate(e_out, d_kernel)
+    return d_out
+def close_image(input_image, e_kernel, d_kernel):
+    d_out = cv.dilate(input_image, d_kernel)
+    e_out = cv.erode(d_out, e_kernel)
+    return e_out
+
+
+# Check distance
+def check_distance(test_data):
+    instruments = ['guitar', 'bass', 'trumpet']
+    feature_data = ['circularity', 'compactness', 'elongation', 'thiness', 'intensity']
+    instrument_data = []
+    for instrument in instruments:
+        instrument_data.append(json.load(open(f'data_{instrument}.json', 'r')))
+    guitar_distance = 0
+    bass_distance = 0
+    trumpet_distance = 0
+
+    for num in range(len(feature_data)):
+        guitar_distance += pow(test_data[num]-np.array(instrument_data[0][feature_data[num]]), 2)
+        bass_distance += pow(test_data[num]-np.array(instrument_data[1][feature_data[num]]), 2)
+        trumpet_distance += pow(test_data[num] - np.array(instrument_data[2][feature_data[num]]), 2)
+
+    guitar_distance = min(np.sqrt(guitar_distance))
+    bass_distance = min(np.sqrt(bass_distance))
+    trumpet_distance = min(np.sqrt(trumpet_distance))
+
+    if guitar_distance < bass_distance and guitar_distance < trumpet_distance:
+        return "guitar"
+    elif bass_distance < guitar_distance and bass_distance < trumpet_distance:
+        return "bass"
+    else:
+        return "trumpet"
+
+
 # importing the pictures
-PicNum = 1
+PicNum = 2
 n_pics = 8
 pictures = np.empty(n_pics, dtype=object)
 grey = pictures
 
 for i in range(len(pictures)):
-    pictures[i] = cv.imread(Path.cwd().as_posix()+'/materialer/trumpet/trumpet'+str(i+1)+'.jpg')
-    grey[i] = cv.imread(Path.cwd().as_posix() + '/materialer/trumpet/trumpet' + str(i + 1) + '.jpg', cv.IMREAD_GRAYSCALE)
+    pictures[i] = cv.imread(Path.cwd().as_posix()+'/materialer/guitar/guitar'+str(i+1)+'.jpg')
+    grey[i] = cv.imread(Path.cwd().as_posix() + '/materialer/guitar/guitar' + str(i + 1) + '.jpg', cv.IMREAD_GRAYSCALE)
 
-ret, threshold = cv.threshold(grey[1], 210, 255, cv.THRESH_BINARY)
-contours, hierarchy = cv.findContours(image=threshold, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_NONE)
-image_copy = pictures[1].copy()
-cv.drawContours(image_copy, contours, -1, (0, 255, 0), 3)
-for i in range(len(contours)):
-    cnt = contours[i]
+blur = cv.medianBlur(grey[PicNum], 21)
+ret, threshold = cv.threshold(blur, 210, 255, cv.THRESH_BINARY_INV)
+closed = close_image(threshold, cv.getStructuringElement(cv.MORPH_ELLIPSE, (15, 15)),
+                     cv.getStructuringElement(cv.MORPH_ELLIPSE, (35, 35)))
+
+# Extract contours
+contours, hierarchy = cv.findContours(image=closed, mode=cv.RETR_EXTERNAL, method=cv.CHAIN_APPROX_NONE)
+
+# Save the contours into the Features class
+for cnt in contours:
+    f = Features(cnt, grey[PicNum])
+    cnt_features = f.return_vector()
+    cnt_type = check_distance(cnt_features)
+    print(cnt_type)
 
 
-
-#     f.contourIndex = i
-#     f.area = cv.contourArea(cnt)
-#     f.perimeter = cv.arcLength(cnt, True)
-#     f.circularity = (4 * np.pi * f.area) / pow(f.perimeter, 2)
-#
-#     RotatedRect, box = cv.minAreaRect(cnt)
-#     f.elongation = max(box.size.width / box.size.height, box.size.height / box.size.width)
-
-resize_image(image_copy, 'threshold', 0.4)
-cv.waitKey(0)
+#resize_image(image_copy, 'threshold', 0.4)
+#cv.waitKey(0)
